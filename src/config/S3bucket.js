1509bed10dda6e818fcfb71da26f9f7474f9befc
adyeetya <2612adityasingh2000@gmail.com>
2025-11-05 16:17:20 +0530

    first commit

--- src/config/S3bucket.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    bucketName: "mobileapplications",
-    dirName: "photo",
-    region: "ap-south-1",
-    accessKeyId: "",
-    secretAccessKey: ""
-  };
-// AKIAQKGGXOAWXVFSQCGZ
-  // xgREbv02Wmany4WR/thupyncI+e2pQHPAHIlZFtA
